a40f5591e1236e5c1572809fa6f576ecc89e23b2
Jayson Hamilton <145177201+Jsyn14@users.noreply.github.com>
2024-03-06 12:14:36 -0600

    Add files via upload

---
+++ AClassScoreAverage.py
@@ -0,0 +1,18 @@
+#Jayson Hamilton 
+# Get five class scores and assign them to the
+# class1, class2, class3, class4 and class5 variables.
+print("  ")
+print("This program will average out 5 classes scores.")
+print("  ")
+class1 = float(input('Enter the first class score: '))
+class2 = float(input('Enter the second class score: '))
+class3 = float(input('Enter the third class score: '))
+class4 = float(input('Enter the fourth class score: '))
+class5 = float(input('Enter the fifth class score: '))
+# Calculate the average of the three scores
+# and assign the result to the average variable.
+average = (class1 + class2 + class3 + class4 + class5) / 5.0
+
+# Display the average.
+
+print('The average score is', average)
